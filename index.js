0f9f33036c3d40e517bae5033669d92fc5772b65
cliff <cliffmudigo5@gmail.com>
2023-04-04 10:39:28 +0300

    css partially done

---
+++ index.js
@@ -0,0 +1,8 @@
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("search-btn");
+let cityRef = document.getElementById("city");
+
+//function to fetch weather details from api and to display them
+let getWeather = () => {
+    let url = 
+}
