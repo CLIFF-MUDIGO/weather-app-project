78797eb76eed0a9d4bc7bc8d4ec93600f87817c9
cliff <cliffmudigo5@gmail.com>
2023-04-04 11:10:46 +0300

    fetch weather details fron api

--- index.js
+++ index.js
@@ -4,5 +4,36 @@ let cityRef = document.getElementById("city");
 
 //function to fetch weather details from api and to display them
 let getWeather = () => {
-    let url = 
-}
+    let cityValue = cityRef.value;
+    //if input field is empty
+    if(cityValue.length == 0){
+        result.innerHTML = `<h3>please enter a city name</h3>`;
+    }
+    //if input field is NOT empty
+    else {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`; 
+    fetch(url).then((resp) => resp.json())
+    //if city name is valid
+    .then(data =>{
+        console.log(data);
+        console.log(data.weather[0].icon);
+        console.log(data.weather[0].main);
+        console.log(data.weather[0].description);
+        console.log(data.name);
+        console.log(data.main.temp_min);
+        console.log(data.main.temp_max);
+        
+        result.innerHTML =
+
+
+
+
+
+
+    })
+    .catch(() =>{
+        result.innerHTML =`<h3>city not found</h3>`;
+    })
+    }
+};
+window.addEventListener("load", getWeather);
--- key.js
+++ key.js
@@ -1,2 +1,2 @@
 //assign the copied API key to the 'key varriable
-key=
+key='140c506384c6d9a3d4ec7996fce7b379';
